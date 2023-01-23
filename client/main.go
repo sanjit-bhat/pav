d5cd9a0bbbdc57c4a85b1afa82ea94579593246f
Sanjit Bhat <sanjit.bhat@gmail.com>
2023-01-23 15:26:20 -0500

    format files

--- client/main.go
+++ client/main.go
@@ -1,42 +1,42 @@
 package main
 
 import (
-    "context"
-    "flag"
-    "log"
-    "time"
-    "os"
+	"context"
+	"flag"
+	"log"
+	"os"
+	"time"
 
-    "google.golang.org/grpc"
-    "google.golang.org/grpc/credentials/insecure"
-    pb "example.com/rpc"
+	pb "example.com/rpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var (
-    addr = flag.String("addr", "localhost:50051", "server address")
-    file = flag.String("file", "frans.jpg", "path to photo file")
+	addr = flag.String("addr", "localhost:50051", "server address")
+	file = flag.String("file", "frans.jpg", "path to photo file")
 )
 
 func main() {
-    flag.Parse()
-    conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-    if err != nil {
-        log.Fatalln("failed to connect: ", err)
-    }
-    defer conn.Close()
-    c := pb.NewSharerClient(conn)
+	flag.Parse()
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalln("failed to connect: ", err)
+	}
+	defer conn.Close()
+	c := pb.NewSharerClient(conn)
 
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-    defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
 
-    fullFile := "client/photos/" + *file
-    data, err := os.ReadFile(fullFile)
-    if err != nil {
-        log.Fatalln("failed to read file: ", err)
-    }
-    resp, err := c.PutPhoto(ctx, &pb.PhotoRequest{File: data})
-    if err != nil {
-        log.Fatalln("could not put photo: ", err)
-    }
-    log.Println("File is saved under: ", resp.GetPath())
+	fullFile := "client/photos/" + *file
+	data, err := os.ReadFile(fullFile)
+	if err != nil {
+		log.Fatalln("failed to read file: ", err)
+	}
+	resp, err := c.PutPhoto(ctx, &pb.PhotoRequest{File: data})
+	if err != nil {
+		log.Fatalln("could not put photo: ", err)
+	}
+	log.Println("File is saved under: ", resp.GetPath())
 }
