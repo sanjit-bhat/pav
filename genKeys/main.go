4eef082bfe13437aa57214cbddbd9b3f2ae50d03
Sanjit Bhat <sanjit.bhat@gmail.com>
2023-02-07 19:46:41 -0500

    gen keys functionality + makefile

--- genKeys/main.go
+++ genKeys/main.go
@@ -1,86 +1,43 @@
 package main
 
 import (
-	"bytes"
-	"crypto"
 	"crypto/rand"
 	"crypto/rsa"
-	"crypto/sha512"
 	"crypto/x509"
 	"fmt"
+	"os"
 
 	pb "example.com/chatGrpc"
 	"google.golang.org/protobuf/proto"
 )
 
 func main() {
-	origPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		fmt.Println("failed rsa key gen:", err)
-		return
+	names := []string{"alice", "bob", "charlie", "danny", "eve"}
+	userKeys := make([]*pb.UserKey, 5)
+
+	for idx := range userKeys {
+		privKey, err := rsa.GenerateKey(rand.Reader, 4096)
+		if err != nil {
+			fmt.Println("failed rsa key gen:", err)
+			return
+		}
+		pubKey := &privKey.PublicKey
+
+		privKeyBytes := x509.MarshalPKCS1PrivateKey(privKey)
+		pubKeyBytes := x509.MarshalPKCS1PublicKey(pubKey)
+
+		userKey := &pb.UserKey{Name: names[idx], PrivKey: privKeyBytes, PubKey: pubKeyBytes}
+		userKeys[idx] = userKey
 	}
-	_ = origPrivKey
-	origPubKey := &origPrivKey.PublicKey
-	_ = origPubKey
 
-	privKeyBytes := x509.MarshalPKCS1PrivateKey(origPrivKey)
-	pubKeyBytes := x509.MarshalPKCS1PublicKey(origPubKey)
-
-	userKey := &pb.UserKey{Name: "alice", PrivKey: privKeyBytes, PubKey: pubKeyBytes}
-	toDisk, err := proto.Marshal(userKey)
+	manyUserKeys := &pb.ManyUserKeys{UserKeys: userKeys}
+	toDisk, err := proto.Marshal(manyUserKeys)
 	if err != nil {
 		fmt.Println("failed to marshal:", err)
 		return
 	}
-	fromDisk := &pb.UserKey{}
-	err = proto.Unmarshal(toDisk, fromDisk)
-	if err != nil {
-		fmt.Println("failed to unmarshal:", err)
-		return
-	}
-
-	newPrivKey, err := x509.ParsePKCS1PrivateKey(fromDisk.GetPrivKey())
-	_ = newPrivKey
-	if err != nil {
-		fmt.Println("failed to parse priv key:", err)
-		return
-	}
-	newPubKey, err := x509.ParsePKCS1PublicKey(fromDisk.GetPubKey())
-	_ = newPubKey
-	if err != nil {
-		fmt.Println("failed to parse pub key:", err)
-		return
-	}
-
-	// CHANGE THESE
-	expPrivKey := newPrivKey
-	expPubKey := origPubKey
-
-	hello := []byte("hello")
-	helloEnc, err := rsa.EncryptOAEP(sha512.New(), rand.Reader, expPubKey, hello, nil)
-	if err != nil {
-		fmt.Println("failed encrypt:", err)
-		return
-	}
-	helloDec, err := rsa.DecryptOAEP(sha512.New(), rand.Reader, expPrivKey, helloEnc, nil)
-	if err != nil {
-		fmt.Println("failed decrypt:", err)
-		return
-	}
-	if !bytes.Equal(hello, helloDec) {
-		fmt.Println("failed dec same as orig msg")
-		return
-	}
-
-	helloHash := sha512.Sum512(hello)
-	sig, err := rsa.SignPSS(rand.Reader, expPrivKey, crypto.SHA512, helloHash[:], nil)
-	if err != nil {
-		fmt.Println("failed sig:", err)
-		return
-	}
-	err = rsa.VerifyPSS(expPubKey, crypto.SHA512, helloHash[:], sig, nil)
-	if err != nil {
-		fmt.Println("failed verify:", err)
+	if err := os.WriteFile("demo_keys", toDisk, 0644); err != nil {
+		fmt.Println("failed to write file:", err)
 		return
 	}
 }
