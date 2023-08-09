c81a18f68c0165bc5217c2493cdf13c3a8642f9a
Sanjit Bhat <sanjit.bhat@gmail.com>
2023-08-09 16:30:05 -0400

    switch server to condvar

--- server/main.go
+++ server/main.go
@@ -11,49 +11,37 @@ import (
 	"google.golang.org/grpc"
 )
 
-type msgsProt struct {
-	mu   sync.Mutex
-	data []*pb.MsgWrap
-}
-
-type mailboxesProt struct {
-	mu   sync.Mutex
-	data map[uname]chan notifT
-}
-
-type uname string
-type notifT struct{}
 type server struct {
 	pb.UnimplementedChatServer
-	seqNum    atomic.Uint64
-	msgs      msgsProt
-	mailboxes mailboxesProt
+	seqNum atomic.Uint64
+	mu     sync.Mutex
+	msgs   []*pb.MsgWrap
+	newMsg sync.Cond
 }
 
 func newServer() *server {
 	serv := &server{}
-	serv.mailboxes.data = make(map[uname]chan notifT)
-
+	serv.newMsg.L = &serv.mu
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalln("failed to listen to port:", err)
 	}
 	grpcServ := grpc.NewServer()
 	pb.RegisterChatServer(grpcServ, serv)
-	log.Println("server listening at ", lis.Addr())
+	log.Println("server listening at:", lis.Addr())
 	if err := grpcServ.Serve(lis); err != nil {
 		log.Fatalln("failed to serve:", err)
 	}
 	return serv
 }
 
+// Send pending msgs. Assumes lock held. Releases lock.
 func (serv *server) sendPending(msgsIdx *int, stream pb.Chat_GetMsgsServer) error {
-	serv.msgs.mu.Lock()
-	msgs := serv.msgs.data
+	msgs := serv.msgs
 	msgsLen := len(msgs)
 	pending := make([]*pb.MsgWrap, msgsLen-*msgsIdx)
 	copy(pending, msgs[*msgsIdx:msgsLen])
-	serv.msgs.mu.Unlock()
+	serv.mu.Unlock()
 
 	for _, msg := range pending {
 		resp := pb.GetMsgsResp{Msg: msg}
@@ -66,20 +54,16 @@ func (serv *server) sendPending(msgsIdx *int, stream pb.Chat_GetMsgsServer) erro
 }
 
 func (serv *server) GetMsgs(in *pb.GetMsgsReq, stream pb.Chat_GetMsgsServer) error {
-	mailbox := make(chan notifT, 10)
-	serv.mailboxes.mu.Lock()
-	serv.mailboxes.data[uname(in.Sender)] = mailbox
-	serv.mailboxes.mu.Unlock()
-
 	msgsIdx := new(int)
+	serv.mu.Lock()
 	if err := serv.sendPending(msgsIdx, stream); err != nil {
 		return err
 	}
 
 	for {
-		_, more := <-mailbox
-		if !more {
-			return nil
+		serv.mu.Lock()
+		for !(*msgsIdx < len(serv.msgs)) {
+			serv.newMsg.Wait()
 		}
 		if err := serv.sendPending(msgsIdx, stream); err != nil {
 			return err
@@ -89,21 +73,12 @@ func (serv *server) GetMsgs(in *pb.GetMsgsReq, stream pb.Chat_GetMsgsServer) err
 
 func (serv *server) PutMsg(ctx context.Context, in *pb.PutMsgReq) (*pb.PutMsgResp, error) {
 	msg := in.Msg
-
 	msg.SeqNum = serv.seqNum.Add(1)
 
-	serv.msgs.mu.Lock()
-	serv.msgs.data = append(serv.msgs.data, msg)
-	serv.msgs.mu.Unlock()
-
-	// This CS should not deadlock.
-	// The channel read is not acquiring the mailbox lock.
-	// It is acquiring the msgs lock, but that should always make progress.
-	serv.mailboxes.mu.Lock()
-	for _, ch := range serv.mailboxes.data {
-		ch <- notifT{}
-	}
-	serv.mailboxes.mu.Unlock()
+	serv.mu.Lock()
+	serv.msgs = append(serv.msgs, msg)
+	serv.newMsg.Broadcast()
+	serv.mu.Unlock()
 
 	return &pb.PutMsgResp{}, nil
 }
