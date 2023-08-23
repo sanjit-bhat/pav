03197ff57aaeca2e284bbeb10042efbfa5a2a31d
Sanjit Bhat <sanjit.bhat@gmail.com>
2023-08-23 13:11:58 -0400

    add sign, verify, msgWrap

--- chat4/ffi.go
+++ chat4/ffi.go
@@ -7,7 +7,32 @@ const (
 	RPCPUT rpcIdT = 2
 )
 
-// Don't have an err ret here bc semantics are to just return junk anyways.
+// Don't have an err ret here bc semantics are to just fill "out" with "len(out)" arbitrary bytes.
 func rpcCall(rpcId rpcIdT, in []byte, out []byte) {}
 
+type signerT struct {
+	key []byte
+}
 
+// Generate signature.
+// The actual implementation would return an err if the signing key is invalid.
+// The sig is 64 bytes.
+func (s *signerT) sign(data []byte) ([]byte, errorT) {
+	return make([]byte, 64), ERRNONE
+}
+
+type verifierT struct {
+	key []byte
+}
+
+// Verify a signature.
+// The actual implementation would return an err if the verifier key is invalid.
+func (v *verifierT) verify(data []byte, sig []byte) bool {
+	return true
+}
+
+func makeKeys() (*signerT, *verifierT) {
+	s := &signerT{key: make([]byte, 64)}
+	v := &verifierT{key: make([]byte, 64)}
+	return s, v
+}
--- chat4/prot.go
+++ chat4/prot.go
@@ -1,5 +1,7 @@
 package chat4
 
+import "sync"
+
 type errorT = bool
 
 const (
@@ -7,58 +9,92 @@ const (
 	ERRSOME bool = true
 )
 
-type protRet struct {
-	err errorT
-	sn1 uint64
-}
-
-//lint:ignore U1000 top level of verif unit.
-func aliceMain() protRet {
+func aliceMain(skAlice *signerT, vkBob *verifierT) (uint64, errorT) {
 	// Event 1.
+	tag1 := uint64(1)
 	body1 := uint64(3948)
-	sn1Scratch := uint64(0)
 	pin1Empt := uint64(0)
-	msg1 := newMsgT(body1, sn1Scratch, pin1Empt)
+	msg1 := newMsgT(tag1, body1, pin1Empt)
 	msg1B := encodeMsgT(msg1)
+	sig1, err1 := skAlice.sign(msg1B) 
+	if err1 {
+		return 0, err1
+	}
+	sn1Scratch := uint64(0)
+	msgWrap1 := newMsgWrapT(msg1, sig1, sn1Scratch)
+	msgWrap1B := encodeMsgWrapT(msgWrap1) 
 	ret1Empt := make([]byte, 0)
-	rpcCall(RPCPUT, msg1B, ret1Empt)
+	rpcCall(RPCPUT, msgWrap1B, ret1Empt)
 
 	// Event 2.1.
 	args2Empt := make([]byte, 0)
-	msg1BRet := newMsgTSlice()
-	rpcCall(RPCGET, args2Empt, msg1BRet)
-	msg1Ret := decodeMsgT(msg1BRet)
+	msgWrap2B := newMsgWrapTSlice()
+	rpcCall(RPCGET, args2Empt, msgWrap2B)
+	msgWrap2, _ := decodeMsgWrapT(msgWrap2B)
 
 	// Event 4.
 	args3Empt := make([]byte, 0)
-	msg2B := newMsgTSlice()
-	rpcCall(RPCGET, args3Empt, msg2B)
-	msg2 := decodeMsgT(msg2B)
-
-	// Event 5.
-	if msg1Ret.sn != msg2.pin {
-		return protRet{err: ERRSOME, sn1: 0}
+	msgWrap3B := newMsgWrapTSlice()
+	rpcCall(RPCGET, args3Empt, msgWrap3B)
+	msgWrap3, _ := decodeMsgWrapT(msgWrap3B)
+	msg3B := encodeMsgT(msgWrap3.msg)
+	ok := vkBob.verify(msg3B, msgWrap3.sig)
+	if !ok {
+		return 0, ERRSOME
 	}
 
-	return protRet{err: ERRNONE, sn1: msg1Ret.sn}
+	// Event 5.
+	if msgWrap2.sn != msgWrap3.msg.pin {
+		return 0, ERRSOME
+	}
+
+	return msgWrap2.sn, ERRNONE
 }
 
-//lint:ignore U1000 top level of verif unit.
-func bobMain() protRet {
+func bobMain(skBob *signerT, vkAlice *verifierT) (uint64, errorT) {
 	// Event 2.2.
 	args1Empt := make([]byte, 0)
-	msg1B := newMsgTSlice()
-	rpcCall(RPCGET, args1Empt, msg1B)
-	msg1 := decodeMsgT(msg1B)
+	msgWrap1B := newMsgWrapTSlice()
+	rpcCall(RPCGET, args1Empt, msgWrap1B)
+	msgWrap1, _ := decodeMsgWrapT(msgWrap1B)
+	msg1B := encodeMsgT(msgWrap1.msg)
+	ok := vkAlice.verify(msg1B, msgWrap1.sig)
+	if !ok {
+		return 0, ERRSOME
+	}
 
 	// Event 3.
+	tag2 := uint64(2)
 	body2 := uint64(8959)
-	sn2Scratch := uint64(0)
 	// Core protocol: msg1's sn becomes pin for msg2.
-	msg2 := newMsgT(body2, sn2Scratch, msg1.sn)
+	msg2 := newMsgT(tag2, body2, msgWrap1.sn)
 	msg2B := encodeMsgT(msg2)
+	sig2, err2 := skBob.sign(msg2B)
+	if err2 {
+		return 0, err2
+	}
+	sn2Scratch := uint64(0)
+	msgWrap2 := newMsgWrapT(msg2, sig2, sn2Scratch)
+	msgWrap2B := encodeMsgWrapT(msgWrap2)
 	ret2Empt := make([]byte, 0)
-	rpcCall(RPCPUT, msg2B, ret2Empt)
+	rpcCall(RPCPUT, msgWrap2B, ret2Empt)
 
-	return protRet{err: ERRNONE, sn1: msg1.sn}
+	return msgWrap1.sn, ERRNONE
+}
+
+//lint:ignore U1000 we probably won't verify this.
+func setup() {
+	skAlice, vkAlice := makeKeys()
+	skBob, vkBob := makeKeys()
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
+	go func() {
+		aliceMain(skAlice, vkBob)
+		wg.Done()
+	}()
+	go func() {
+		bobMain(skBob, vkAlice)
+		wg.Done()
+	}()
+	wg.Wait()
 }
