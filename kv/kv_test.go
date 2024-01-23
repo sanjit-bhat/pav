9a3ada904309715f58e55b63906f7f664eaab590
Sanjit Bhat <sanjit.bhat@gmail.com>
2024-01-23 18:22:06 -0500

    add porcupine test

--- kv/kv_test.go
+++ kv/kv_test.go
@@ -2,20 +2,21 @@ package kv
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/mit-pdos/gokv/grove_ffi"
 	"github.com/mit-pdos/secure-chat/kv/ffi"
+	"math/rand"
 	"sync"
 	"testing"
 	"time"
 )
 
 func TestBasic(t *testing.T) {
-	c := ffi.Init()
 	numUsers := 2
 	sks := make([]*ffi.SignerT, numUsers)
 	vks := make([]*ffi.VerifierT, numUsers)
 	for i := 0; i < numUsers; i++ {
-		sks[i], vks[i] = c.MakeKeys()
+		sks[i], vks[i] = ffi.MakeKeys()
 	}
 
 	addr := grove_ffi.MakeAddress("0.0.0.0:6060")
@@ -27,8 +28,8 @@ func TestBasic(t *testing.T) {
 	oneDone := make(chan struct{}, 1)
 	twoDone := make(chan struct{}, 1)
 	k := uint64(3)
-	v1 := []byte("value1")
-	v2 := []byte("value2")
+	v1 := []byte("v1")
+	v2 := []byte("v2")
 
 	wg.Add(1)
 	go func() {
@@ -42,7 +43,7 @@ func TestBasic(t *testing.T) {
 		<-twoDone
 		v3 := c.Get(k)
 		if !bytes.Equal(v2, v3) {
-			t.Errorf("fail")
+			t.Fail()
 		}
 	}()
 	wg.Add(1)
@@ -54,7 +55,7 @@ func TestBasic(t *testing.T) {
 		<-oneDone
 		v3 := c.Get(k)
 		if !bytes.Equal(v1, v3) {
-			t.Errorf("fail")
+			t.Fail()
 		}
 		c.Put(k, v2)
 		twoDone <- struct{}{}
@@ -62,4 +63,51 @@ func TestBasic(t *testing.T) {
 
 	wg.Wait()
 	// TODO: will Go garbage collect the server thread here?
+}
+
+func TestPorcupine(t *testing.T) {
+	numUsers := 10
+	numKeys := 10
+	numVals := 10
+	numOps := 1000
+
+	sks := make([]*ffi.SignerT, numUsers)
+	vks := make([]*ffi.VerifierT, numUsers)
+	for i := 0; i < numUsers; i++ {
+		sks[i], vks[i] = ffi.MakeKeys()
+	}
+	vals := make([][]byte, numVals)
+	for i := 0; i < numVals; i++ {
+		vals[i] = []byte(fmt.Sprintf("v%d", i))
+	}
+
+	addr := grove_ffi.MakeAddress("0.0.0.0:6061")
+	serverStartup := time.Millisecond
+	s := ffi.MakeServer()
+	s.Start(addr)
+
+	var wg sync.WaitGroup
+	for i := 0; i < numUsers; i++ {
+		wg.Add(1)
+		go func(uid int) {
+			defer wg.Done()
+			time.Sleep(serverStartup)
+			c := MakeKvCli(addr, sks[uid], vks, uint64(uid))
+
+			for opIdx := 0; opIdx < numOps; opIdx++ {
+				if opIdx%100 == 0 {
+					fmt.Println("uid", uid, "opIdx", opIdx)
+				}
+				k := uint64(rand.Intn(numKeys))
+				op := rand.Intn(2)
+				if op == 0 {
+					c.Get(k)
+				} else {
+					v := vals[rand.Intn(numVals)]
+					c.Put(k, v)
+				}
+			}
+		}(i)
+	}
+	wg.Wait()
 }
