baf2fff77d8dd4ccaf7eb2a56bf0f55337ef91b6
Sanjit Bhat <sanjit.bhat@gmail.com>
2024-03-03 17:24:59 -0500

    stub out auth_log

---
+++ auth_log/auth_log.go
@@ -0,0 +1,57 @@
+package auth_log
+
+type AuthLog struct {
+}
+
+func NewAuthLog() *AuthLog {
+	return &AuthLog{}
+}
+
+type Entry struct {
+}
+
+type InclusionProof struct {
+}
+
+type ExtensionProof struct {
+}
+
+type EpochNum uint64
+
+type Digest struct {
+}
+
+// Log API.
+
+// InclusionProof should have latest merkle root,
+// so caller can fast-forward itself.
+func (l *AuthLog) Put(e *Entry) *InclusionProof {
+	panic("todo")
+}
+
+// Assuming InclusionProof talks about id-value,
+// so don't need to return that by itself.
+func (l *AuthLog) Get() *InclusionProof {
+	panic("todo")
+}
+
+// Will special case certain values, e.g.,
+// a 0 end might mean "up until latest epoch".
+func (l *AuthLog) GetHist(start, end EpochNum) []*InclusionProof {
+	panic("todo")
+}
+
+// Not sure if this proof is really just list of inclusion proofs.
+func (l *AuthLog) GetExtend(start, end EpochNum) *ExtensionProof {
+	panic("todo")
+}
+
+// Authentication API.
+
+func CheckInclusion(proof *InclusionProof) bool {
+	panic("todo")
+}
+
+func CheckExtension(proof *ExtensionProof) bool {
+    panic("todo")
+}
