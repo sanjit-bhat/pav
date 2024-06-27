ba993c5de4a419fbd5bb07a037363d9f8c36b2ec
Sanjit Bhat <sanjit.bhat@gmail.com>
2024-06-27 17:38:35 -0400

    shorten var names

--- etc/ci-check.py
+++ etc/ci-check.py
@@ -7,14 +7,14 @@ proj_root = Path(__file__).parents[1]
 
 class Tests(unittest.TestCase):
     def test_ktmerkle_compiled(self):
-        command = "go run ./rpc --in ktmerkle/rpc.go && git diff --exit-code ktmerkle/rpc.out.go"
-        result = subprocess.run(
-            command, cwd=proj_root, shell=True, capture_output=True, text=True
+        cmd = "go run ./rpc --in ktmerkle/rpc.go && git diff --exit-code ktmerkle/rpc.out.go"
+        res = subprocess.run(
+            cmd, cwd=proj_root, shell=True, capture_output=True, text=True
         )
         self.assertEqual(
-            result.returncode,
+            res.returncode,
             0,
-            f"Command failed with error message: {result.stderr} and output: {result.stdout}",
+            f"Command failed with error message: {res.stderr} and output: {res.stdout}",
         )
 
 
