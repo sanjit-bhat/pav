95f5b33397fd1c663e710925ce0f673abe970b22
Sanjit Bhat <sanjit.bhat@gmail.com>
2024-10-20 16:46:15 -0400

    put locked state inside struct, more intuitive

--- kt/test.go
+++ kt/test.go
@@ -33,30 +33,27 @@ func testAll(setup *setupParams) {
 		syncAdtrs(setup.servAddr, setup.adtrAddrs)
 	}()
 
-	// run alice and bob.
-	alice := &alice{}
-	aliceMu := new(sync.Mutex)
-	aliceMu.Lock()
+	// alice does a bunch of puts.
 	aliceCli := newClient(aliceUid, setup.servAddr, setup.servSigPk, setup.servVrfPk)
+	alice := &alice{mu: new(sync.Mutex), cli: aliceCli, hist: nil}
+	alice.mu.Lock()
 	go func() {
-		alice.run(aliceCli)
-		aliceMu.Unlock()
+		alice.run()
 	}()
-	bob := &bob{}
-	bobMu := new(sync.Mutex)
-	bobMu.Lock()
+	// bob does a get.
 	bobCli := newClient(bobUid, setup.servAddr, setup.servSigPk, setup.servVrfPk)
+	bob := &bob{mu: new(sync.Mutex), cli: bobCli, epoch: 0, isReg: false, alicePk: nil}
+	bob.mu.Lock()
 	go func() {
-		bob.run(bobCli)
-		bobMu.Unlock()
+		bob.run()
 	}()
 
 	// wait for alice and bob to finish.
-	aliceMu.Lock()
-	bobMu.Lock()
+	alice.mu.Lock()
+	bob.mu.Lock()
 
 	// alice self monitor. in real world, she'll come on-line at times and do this.
-	selfMonEp, err0 := aliceCli.SelfMon()
+	selfMonEp, err0 := alice.cli.SelfMon()
 	primitive.Assume(!err0.err)
 	// this last self monitor will be our history bound.
 	primitive.Assume(bob.epoch <= selfMonEp)
@@ -64,9 +61,9 @@ func testAll(setup *setupParams) {
 	// wait for auditors to catch all updates.
 	primitive.Sleep(1000_000_000)
 
-	// alice and bob Audit. ordering irrelevant across clients.
-	doAudits(aliceCli, setup.adtrAddrs, setup.adtrPks)
-	doAudits(bobCli, setup.adtrAddrs, setup.adtrPks)
+	// alice and bob audit. ordering irrelevant across clients.
+	doAudits(alice.cli, setup.adtrAddrs, setup.adtrPks)
+	doAudits(bob.cli, setup.adtrAddrs, setup.adtrPks)
 
 	// final check. bob got the right key.
 	isReg, aliceKey := GetHist(alice.hist, bob.epoch)
@@ -77,32 +74,38 @@ func testAll(setup *setupParams) {
 }
 
 type alice struct {
+	mu   *sync.Mutex
+	cli  *Client
 	hist []*HistEntry
 }
 
-func (a *alice) run(cli *Client) {
+func (a *alice) run() {
 	for i := uint64(0); i < uint64(20); i++ {
 		primitive.Sleep(50_000_000)
 		pk := []byte{byte(i)}
-		epoch, err0 := cli.Put(pk)
+		epoch, err0 := a.cli.Put(pk)
 		primitive.Assume(!err0.err)
 		a.hist = append(a.hist, &HistEntry{Epoch: epoch, HistVal: pk})
 	}
+	a.mu.Unlock()
 }
 
 type bob struct {
+	mu      *sync.Mutex
+	cli     *Client
 	epoch   uint64
 	isReg   bool
 	alicePk []byte
 }
 
-func (b *bob) run(cli *Client) {
+func (b *bob) run() {
 	primitive.Sleep(550_000_000)
-	isReg, pk, epoch, err0 := cli.Get(aliceUid)
+	isReg, pk, epoch, err0 := b.cli.Get(aliceUid)
 	primitive.Assume(!err0.err)
 	b.epoch = epoch
 	b.isReg = isReg
 	b.alicePk = pk
+	b.mu.Unlock()
 }
 
 // chaos from Charlie running ops.
