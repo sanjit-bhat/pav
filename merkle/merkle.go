2a381b02b7aef3f1fb8f18de08fc8108b179dde6
Sanjit Bhat <sanjit.bhat@gmail.com>
2024-12-01 16:33:36 -0500

    merkle: more cleanup

--- merkle/merkle.go
+++ merkle/merkle.go
@@ -16,12 +16,9 @@ const (
 	MembProofTy     bool   = true
 )
 
-// context is a result of:
-// 1) for performance reasons, wanting to pre-compute the empty hash.
-// 2) requiring that all hashes come from program steps.
-// 3) goose not having init() support.
-type context struct {
-	emptyHash []byte
+type Tree struct {
+	ctx  *context
+	root *node
 }
 
 type node struct {
@@ -30,211 +27,16 @@ type node struct {
 	children []*node
 }
 
-func newCtx() *context {
-	// hash of the empty node.
-	return &context{emptyHash: cryptoffi.Hash([]byte{emptyNodeTag})}
-}
-
-// getHash getter to support hashes of empty (nil) nodes.
-func (ctx *context) getHash(n *node) []byte {
-	if n == nil {
-		return ctx.emptyHash
-	}
-	return n.hash
-}
-
-func (n *node) updateLeafHash() {
-	var h cryptoutil.Hasher
-	// TODO: tag needs to go before val?
-	cryptoutil.HasherWrite(&h, n.mapVal)
-	cryptoutil.HasherWrite(&h, []byte{leafNodeTag})
-	n.hash = cryptoutil.HasherSum(h, nil)
-}
-
-// Assumes recursive child hashes are already up-to-date.
-func (ctx *context) updateInteriorHash(n *node) {
-	var h cryptoutil.Hasher
-	for _, child := range n.children {
-		cryptoutil.HasherWrite(&h, ctx.getHash(child))
-	}
-	cryptoutil.HasherWrite(&h, []byte{interiorNodeTag})
-	n.hash = cryptoutil.HasherSum(h, nil)
-}
-
-// This node doesn't satisfy the invariant for any logical node.
-// It'll be specialized after adding it to the tree.
-func newGenericNode() *node {
-	c := make([]*node, numChildren)
-	return &node{children: c}
-}
-
-// General proof object.
-// Binds a label down the tree to a particular node hash.
-type pathProof struct {
-	label       []byte
-	nodeHash    []byte
-	digest      []byte
-	childHashes [][][]byte
-}
-
-func isValidHashSl(data [][]byte) bool {
-	var ok = true
-	for _, hash := range data {
-		if uint64(len(hash)) != cryptoffi.HashLen {
-			ok = false
-		}
-	}
-	return ok
-}
-
-func (p *pathProof) check() bool {
-	var err = false
-	var currHash []byte = p.nodeHash
-	proofLen := uint64(len(p.childHashes))
-	// Goose doesn't support general loops, so re-write this way.
-	// TODO: try writing with general loop syntax.
-	var loopIdx = uint64(0)
-	for ; loopIdx < proofLen; loopIdx++ {
-		pathIdx := proofLen - 1 - loopIdx
-		children := p.childHashes[pathIdx]
-		if uint64(len(children)) != numChildren-1 {
-			err = true
-			continue
-		}
-		if !isValidHashSl(children) {
-			err = true
-			continue
-		}
-		pos := uint64(p.label[pathIdx])
-		before := children[:pos]
-		after := children[pos:]
-
-		var hr cryptoutil.Hasher
-		cryptoutil.HasherWriteSl(&hr, before)
-		cryptoutil.HasherWrite(&hr, currHash)
-		cryptoutil.HasherWriteSl(&hr, after)
-		cryptoutil.HasherWrite(&hr, []byte{interiorNodeTag})
-		currHash = cryptoutil.HasherSum(hr, nil)
-	}
-
-	if err {
-		return true
-	}
-	if !std.BytesEqual(currHash, p.digest) {
-		return true
-	}
-	return false
-}
-
-func getLeafNodeHash(mapVal []byte) []byte {
-	var hr cryptoutil.Hasher
-	cryptoutil.HasherWrite(&hr, mapVal)
-	cryptoutil.HasherWrite(&hr, []byte{leafNodeTag})
-	return cryptoutil.HasherSum(hr, nil)
-}
-
-func getEmptyNodeHash() []byte {
-	return cryptoffi.Hash([]byte{emptyNodeTag})
-}
-
-func CheckProof(proofTy bool, proof [][][]byte, label []byte, mapVal []byte, digest []byte) bool {
-	if uint64(len(proof)) > cryptoffi.HashLen {
-		return true
-	}
-	if len(label) < len(proof) {
-		return true
-	}
-	// For NonmembProof, have original label, so slice it down
-	// to same sz as path.
-	labelPref := label[:len(proof)]
-	var nodeHash []byte
-	if proofTy {
-		nodeHash = getLeafNodeHash(mapVal)
-	} else {
-		nodeHash = getEmptyNodeHash()
-	}
-
-	pathProof := &pathProof{
-		label:       labelPref,
-		nodeHash:    nodeHash,
-		digest:      digest,
-		childHashes: proof,
-	}
-	return pathProof.check()
-}
-
-// Having a separate Tree type makes the API more clear compared to if it
-// was just a Node.
-type Tree struct {
-	ctx  *context
-	root *node
-}
-
-func NewTree() *Tree {
-	return &Tree{ctx: newCtx()}
+// context is a result of:
+// 1) for performance reasons, wanting to pre-compute the empty hash.
+// 2) requiring that all hashes come from program steps.
+// 3) goose not having init() support.
+type context struct {
+	emptyHash []byte
 }
 
 func (t *Tree) Digest() []byte {
 	return t.ctx.getHash(t.root)
-}
-
-func (ctx *context) appendNode2D(dst *[][]byte, src []*node) {
-	for _, n := range src {
-		*dst = append(*dst, ctx.getHash(n))
-	}
-}
-
-func (ctx *context) getChildHashes(nodePath []*node, label []byte) [][][]byte {
-	childHashes := make([][][]byte, len(nodePath)-1)
-	for pathIdx := uint64(0); pathIdx < uint64(len(nodePath))-1; pathIdx++ {
-		children := nodePath[pathIdx].children
-		// had a bug where w/o uint64, pos+1 would overflow byte.
-		pos := uint64(label[pathIdx])
-		var proofChildren [][]byte
-		ctx.appendNode2D(&proofChildren, children[:pos])
-		ctx.appendNode2D(&proofChildren, children[pos+1:])
-		childHashes[pathIdx] = proofChildren
-	}
-	return childHashes
-}
-
-// Get the maximal path corresponding to label.
-// If the full path to a leaf node doesn't exist,
-// return the partial path that ends in an empty node.
-func (t *Tree) getPath(label []byte) []*node {
-	var nodePath []*node
-	nodePath = append(nodePath, t.root)
-	if t.root == nil {
-		return nodePath
-	}
-	var stop = false
-	for pathIdx := uint64(0); pathIdx < cryptoffi.HashLen && !stop; pathIdx++ {
-		currNode := nodePath[pathIdx]
-		pos := label[pathIdx]
-		nextNode := currNode.children[pos]
-		nodePath = append(nodePath, nextNode)
-		if nextNode == nil {
-			stop = true
-		}
-	}
-	return nodePath
-}
-
-func (t *Tree) getPathAddNodes(label []byte) []*node {
-	if t.root == nil {
-		t.root = newGenericNode()
-	}
-	var nodePath []*node
-	nodePath = append(nodePath, t.root)
-	for pathIdx := uint64(0); pathIdx < cryptoffi.HashLen; pathIdx++ {
-		currNode := nodePath[pathIdx]
-		pos := uint64(label[pathIdx])
-		if currNode.children[pos] == nil {
-			currNode.children[pos] = newGenericNode()
-		}
-		nodePath = append(nodePath, currNode.children[pos])
-	}
-	return nodePath
 }
 
 // Put returns the digest, proof, and error.
@@ -245,7 +47,7 @@ func (t *Tree) Put(label []byte, mapVal []byte) ([]byte, [][][]byte, bool) {
 
 	nodePath := t.getPathAddNodes(label)
 	nodePath[cryptoffi.HashLen].mapVal = mapVal
-	nodePath[cryptoffi.HashLen].updateLeafHash()
+	nodePath[cryptoffi.HashLen].hash = getLeafNodeHash(mapVal)
 	// +1/-1 offsets for Goosable uint64 loop var.
 	for pathIdx := cryptoffi.HashLen; pathIdx >= 1; pathIdx-- {
 		t.ctx.updateInteriorHash(nodePath[pathIdx-1])
@@ -273,4 +75,170 @@ func (t *Tree) Get(label []byte) ([]byte, []byte, bool, [][][]byte, bool) {
 		val := lastNode.mapVal
 		return val, digest, MembProofTy, proof, false
 	}
+}
+
+func NewTree() *Tree {
+	return &Tree{ctx: newCtx()}
+}
+
+func CheckProof(proofTy bool, proof [][][]byte, label []byte, mapVal []byte, digest []byte) bool {
+	proofLen := uint64(len(proof))
+	if proofLen > cryptoffi.HashLen {
+		return true
+	}
+	if uint64(len(label)) < proofLen {
+		return true
+	}
+	// For NonmembProof, have original label, so slice it down
+	// to same sz as path.
+	labelPref := label[:len(proof)]
+	var nodeHash []byte
+	if proofTy {
+		nodeHash = getLeafNodeHash(mapVal)
+	} else {
+		nodeHash = getEmptyNodeHash()
+	}
+
+	var loopErr = false
+	var loopCurrHash []byte = nodeHash
+	var loopIdx = uint64(0)
+	for ; loopIdx < proofLen; loopIdx++ {
+		pathIdx := proofLen - 1 - loopIdx
+		children := proof[pathIdx]
+		if uint64(len(children)) != numChildren-1 {
+			loopErr = true
+			continue
+		}
+		if !isValidHashSl(children) {
+			loopErr = true
+			continue
+		}
+		pos := uint64(labelPref[pathIdx])
+		before := children[:pos]
+		after := children[pos:]
+
+		var hr cryptoutil.Hasher
+		cryptoutil.HasherWriteSl(&hr, before)
+		cryptoutil.HasherWrite(&hr, loopCurrHash)
+		cryptoutil.HasherWriteSl(&hr, after)
+		cryptoutil.HasherWrite(&hr, []byte{interiorNodeTag})
+		loopCurrHash = cryptoutil.HasherSum(hr, nil)
+	}
+
+	if loopErr {
+		return true
+	}
+	if !std.BytesEqual(loopCurrHash, digest) {
+		return true
+	}
+	return false
+}
+
+func getEmptyNodeHash() []byte {
+	return cryptoffi.Hash([]byte{emptyNodeTag})
+}
+
+func getLeafNodeHash(mapVal []byte) []byte {
+	var hr cryptoutil.Hasher
+	cryptoutil.HasherWrite(&hr, mapVal)
+	cryptoutil.HasherWrite(&hr, []byte{leafNodeTag})
+	return cryptoutil.HasherSum(hr, nil)
+}
+
+func newCtx() *context {
+	return &context{emptyHash: getEmptyNodeHash()}
+}
+
+// getHash getter to support hashes of empty (nil) nodes.
+func (ctx *context) getHash(n *node) []byte {
+	if n == nil {
+		return ctx.emptyHash
+	}
+	return n.hash
+}
+
+// Assumes recursive child hashes are already up-to-date.
+func (ctx *context) updateInteriorHash(n *node) {
+	var h cryptoutil.Hasher
+	for _, child := range n.children {
+		cryptoutil.HasherWrite(&h, ctx.getHash(child))
+	}
+	cryptoutil.HasherWrite(&h, []byte{interiorNodeTag})
+	n.hash = cryptoutil.HasherSum(h, nil)
+}
+
+// Get the maximal path corresponding to label.
+// If the full path to a leaf node doesn't exist,
+// return the partial path that ends in an empty node.
+func (t *Tree) getPath(label []byte) []*node {
+	var nodePath []*node
+	nodePath = append(nodePath, t.root)
+	if t.root == nil {
+		return nodePath
+	}
+	var stop = false
+	for pathIdx := uint64(0); pathIdx < cryptoffi.HashLen && !stop; pathIdx++ {
+		currNode := nodePath[pathIdx]
+		pos := label[pathIdx]
+		nextNode := currNode.children[pos]
+		nodePath = append(nodePath, nextNode)
+		if nextNode == nil {
+			stop = true
+		}
+	}
+	return nodePath
+}
+
+// This node doesn't satisfy the invariant for any logical node.
+// It'll be specialized after adding it to the tree.
+func newGenericNode() *node {
+	c := make([]*node, numChildren)
+	return &node{children: c}
+}
+
+func (t *Tree) getPathAddNodes(label []byte) []*node {
+	if t.root == nil {
+		t.root = newGenericNode()
+	}
+	var nodePath []*node
+	nodePath = append(nodePath, t.root)
+	for pathIdx := uint64(0); pathIdx < cryptoffi.HashLen; pathIdx++ {
+		currNode := nodePath[pathIdx]
+		pos := uint64(label[pathIdx])
+		if currNode.children[pos] == nil {
+			currNode.children[pos] = newGenericNode()
+		}
+		nodePath = append(nodePath, currNode.children[pos])
+	}
+	return nodePath
+}
+
+func (ctx *context) getChildHashes(nodePath []*node, label []byte) [][][]byte {
+	childHashes := make([][][]byte, len(nodePath)-1)
+	for pathIdx := uint64(0); pathIdx < uint64(len(nodePath))-1; pathIdx++ {
+		children := nodePath[pathIdx].children
+		// had a bug where w/o uint64, pos+1 would overflow byte.
+		pos := uint64(label[pathIdx])
+		var proofChildren [][]byte
+		ctx.appendNode2D(&proofChildren, children[:pos])
+		ctx.appendNode2D(&proofChildren, children[pos+1:])
+		childHashes[pathIdx] = proofChildren
+	}
+	return childHashes
+}
+
+func (ctx *context) appendNode2D(dst *[][]byte, src []*node) {
+	for _, n := range src {
+		*dst = append(*dst, ctx.getHash(n))
+	}
+}
+
+func isValidHashSl(data [][]byte) bool {
+	var ok = true
+	for _, hash := range data {
+		if uint64(len(hash)) != cryptoffi.HashLen {
+			ok = false
+		}
+	}
+	return ok
 }
