0505581983216dc30760f8c53655a42f3e4c14d4
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-01-15 15:16:02 -0500

    add depth debug info. but still not able to explain perf diff

--- merkle/bench_test.go
+++ merkle/bench_test.go
@@ -51,14 +51,15 @@ func BenchmarkGetMemb(b *testing.B) {
 	}
 }
 
-func BenchmarkGetNonMembExisting(b *testing.B) {
+func BenchmarkGetNonMembWithPuts(b *testing.B) {
 	tree := NewTree()
 	val := []byte("val")
 	var seed [32]byte
 	rnd := rand.NewChaCha8(seed)
 	label := make([]byte, cryptoffi.HashLen)
+	totalDep := 0
+	total := 0
 
-	b.StopTimer()
 	for i := 0; i < 500; i++ {
 		_, err := rnd.Read(label)
 		if err != nil {
@@ -69,19 +70,25 @@ func BenchmarkGetNonMembExisting(b *testing.B) {
 			b.Fatal()
 		}
 	}
-	b.StartTimer()
 
+	b.Log("starting new loop")
+	b.ResetTimer()
 	for range b.N {
 		_, err := rnd.Read(label)
 		if err != nil {
 			b.Fatal(err)
 		}
 
-		_, _, _, _, errb := tree.Get(label)
+		_, _, _, proof, errb := tree.Get(label)
 		if errb {
 			b.Fatal()
 		}
+		depth := uint64(len(proof)) / hashesPerProofDepth
+		totalDep += int(depth)
+		total++
 	}
+
+	b.Log("avg dep", float32(totalDep) / float32(total), "total", total)
 }
 
 func BenchmarkGetNonMemb(b *testing.B) {
@@ -89,16 +96,24 @@ func BenchmarkGetNonMemb(b *testing.B) {
 	var seed [32]byte
 	rnd := rand.NewChaCha8(seed)
 	label := make([]byte, cryptoffi.HashLen)
+	nonEmpt := 0
 
+	b.ResetTimer()
 	for range b.N {
 		_, err := rnd.Read(label)
 		if err != nil {
 			b.Fatal(err)
 		}
 
-		_, _, _, _, errb := tree.Get(label)
+		_, _, _, proof, errb := tree.Get(label)
 		if errb {
 			b.Fatal()
 		}
+		depth := uint64(len(proof)) / hashesPerProofDepth
+		if depth != 0 {
+			nonEmpt++
+		}
 	}
+
+	b.Log(nonEmpt)
 }
