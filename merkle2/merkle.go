b2ddb33dc0d8b3925bfe103454762cb28343d323
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-01-22 14:53:46 -0500

    implement merkle with 1) no prefix in interior and 2) prefix in leaf

--- merkle2/merkle.go
+++ merkle2/merkle.go
@@ -1,6 +1,8 @@
 package merkle
 
 import (
+	"github.com/goose-lang/primitive"
+	"github.com/goose-lang/std"
 	"github.com/mit-pdos/pav/cryptoffi"
 	"github.com/tchajed/marshal"
 )
@@ -11,60 +13,23 @@ const (
 	leafNodeTag     byte = 2
 )
 
-type bits []bool
-
-func newBitsFromBytes(b []byte) bits {
-	var bs = make([]bool, 0, 8*len(b))
-	for _, x := range b {
-		for i := 0; i < 8; i++ {
-			bs = append(bs, x&(1<<i) != 0)
-		}
-	}
-	return bs
-}
-
-func boolToByte(b bool) byte {
-	if b {
-		return 1
-	} else {
-		return 0
-	}
-}
-
-func (b bits) encode() []byte {
-	var bs []byte
-	var i int
-	var byt byte
-	// TODO: encode and decode might not be inverses.
-	// but maybe that's not important.
-	for _, x := range b {
-		byt |= boolToByte(x) << i
-		i += 1
-		if i == 8 {
-			bs = append(bs, byt)
-			i = 0
-			byt = 0
-		}
-	}
-	return bs
-}
-
 type Tree struct {
 	cache *cache
 	root  *node
 }
 
 // node contains the union of different node types, which distinguish as:
-//  1. empty node. if has nil node ptr.
-//  2. interior node. if has child0 and child1. has prefix and hash.
-//  3. leaf node. else. has prefix, hash, and val.
+//  1. empty node. if node ptr is nil.
+//  2. interior node. if either child0 or child1 not nil. has hash.
+//  3. leaf node. else. has hash, full label, and val.
 type node struct {
-	prefix bits
-	hash   []byte
+	hash []byte
 	// only for interior node.
 	child0 *node
 	// only for interior node.
 	child1 *node
+	// only for leaf node.
+	label []byte
 	// only for leaf node.
 	val []byte
 }
@@ -73,52 +38,75 @@ type cache struct {
 	emptyHash []byte
 }
 
-type updEntry struct {
-	label bits
-	val   []byte
-}
-
-// updSet has across all entries, labels being same len.
-type updSet []*updEntry
-
-// splitSelf gets the longest prefix and splits entries into 0 and 1 post-bits.
-// the updated entries will have the prefix bits removed.
-func (u updSet) splitSelf() (bits, updSet, updSet) {
-	// TODO
-	return nil, nil, nil
-}
-
-// splitOther uses prefix to split entries into 0 and 1 post-bits.
-// the updated entries will have the prefix bits removed.
-func (u updSet) splitOther(prefix bits) (updSet, updSet) {
-	// TODO
-	_ = prefix
-	return nil, nil
-}
-
-// Put adds (label, val) to the tree.
-func (t *Tree) Put(label []byte, val []byte) {
-}
-
-func mkNode(upd updSet, cache *cache) *node {
-	if len(upd) == 0 {
-		// empty node.
-		return nil
+// Put adds (label, val) to the tree and errors if label isn't a hash.
+func (t *Tree) Put(label []byte, val []byte) bool {
+	if uint64(len(label)) != cryptoffi.HashLen {
+		return true
 	}
-	if len(upd) == 1 {
-		// leaf node.
-		u := upd[0]
-		n := &node{prefix: u.label, val: u.val}
-		setLeafHash(n)
-		return n
+	put(&t.root, 0, label, val, t.cache)
+	return false
+}
+
+// Get returns if label is in the tree and, if so, the val.
+// it errors if label isn't a hash.
+func (t *Tree) Get(label []byte) (bool, []byte, bool) {
+	if uint64(len(label)) != cryptoffi.HashLen {
+		return false, nil, true
 	}
-	// interior node.
-	prefix, zero, one := upd.splitSelf()
-	child0 := mkNode(zero, cache)
-	child1 := mkNode(one, cache)
-	n := &node{prefix: prefix, child0: child0, child1: child1}
+	var n = t.root
+	var depth uint64
+	for ; depth < cryptoffi.HashLen*8; depth++ {
+		// break on nil or leaf node.
+		if n == nil || (n.child0 == nil && n.child1 == nil) {
+			break
+		}
+		n = *getChild(n, label, depth)
+	}
+	// empty node.
+	if n == nil {
+		return false, nil, false
+	}
+	// not interior node. can't go depth 256 down and still have interior.
+	primitive.Assert(n.child0 == nil && n.child1 == nil)
+	// leaf node.
+	if !std.BytesEqual(n.label, label) {
+		return false, nil, false
+	}
+	return true, n.val, false
+}
+
+func put(n0 **node, depth uint64, label, val []byte, cache *cache) {
+	n := *n0
+	// empty node.
+	if n == nil {
+		// replace with leaf node.
+		leaf := &node{label: label, val: val}
+		*n0 = leaf
+		setLeafHash(leaf)
+		return
+	}
+
+	// leaf node.
+	if n.child0 == nil && n.child1 == nil {
+		// on exact label match, replace val.
+		if std.BytesEqual(n.label, label) {
+			n.val = val
+			setLeafHash(n)
+			return
+		}
+
+		// otherwise, replace with interior node and recurse.
+		inter := &node{}
+		*n0 = inter
+		*getChild(inter, n.label, depth) = n
+		put(getChild(inter, label, depth), depth+1, label, val, cache)
+		setInteriorHash(inter, cache)
+		return
+	}
+
+	// interior node. recurse.
+	put(getChild(n, label, depth), depth+1, label, val, cache)
 	setInteriorHash(n, cache)
-	return n
 }
 
 func getNodeHash(n *node, c *cache) []byte {
@@ -134,23 +122,36 @@ func compEmptyHash() []byte {
 }
 
 func setLeafHash(n *node) {
-	pref := n.prefix.encode()
-	var b = make([]byte, 0, 8+len(pref)+8+len(n.val)+1)
-	b = marshal.WriteInt(b, uint64(len(pref)))
-	b = append(b, pref...)
-	b = marshal.WriteInt(b, uint64(len(n.val)))
+	valLen := uint64(len(n.val))
+	// TODO: need depth here?
+	var b = make([]byte, 0, cryptoffi.HashLen+8+valLen+1)
+	b = append(b, n.label...)
+	b = marshal.WriteInt(b, valLen)
 	b = append(b, n.val...)
 	b = append(b, leafNodeTag)
 	n.hash = cryptoffi.Hash(b)
 }
 
 func setInteriorHash(n *node, c *cache) {
-	pref := n.prefix.encode()
-	var b = make([]byte, 0, 8+uint64(len(pref))+2*cryptoffi.HashLen+1)
-	b = marshal.WriteInt(b, uint64(len(pref)))
-	b = append(b, pref...)
+	// TODO: need depth here?
+	var b = make([]byte, 0, 2*cryptoffi.HashLen+1)
 	b = append(b, getNodeHash(n.child0, c)...)
 	b = append(b, getNodeHash(n.child1, c)...)
 	b = append(b, interiorNodeTag)
 	n.hash = cryptoffi.Hash(b)
+}
+
+func getChild(n *node, b []byte, depth uint64) **node {
+	if !getBit(b, depth) {
+		return &n.child0
+	} else {
+		return &n.child1
+	}
+}
+
+func getBit(b []byte, n uint64) bool {
+	slot := n / 8
+	off := n % 8
+	x := b[slot]
+	return x&(1<<off) != 0
 }
