b8b6881379a82bf5089e80024a20c3cfa33e63e0
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-02-10 10:19:26 -0500

    separate benchutil into sep pkg. re-using for kt and merkle

---
+++ benchutil/benchutil.go
@@ -0,0 +1,68 @@
+package benchutil
+
+import (
+	"fmt"
+	"io"
+	"math"
+	"runtime"
+	"strings"
+)
+
+type Metric struct {
+	N    float64
+	Unit string
+}
+
+// callerName gives the function name for the caller,
+// after skip frames (where 0 means the current function).
+func callerName(skip int) string {
+	pcs := make([]uintptr, 1)
+	callers := runtime.Callers(skip+2, pcs) // skip + runtime.Callers + callerName
+	if callers == 0 {
+		panic("bench: zero callers found")
+	}
+	frames := runtime.CallersFrames(pcs)
+	frame, _ := frames.Next()
+	fullName := frame.Function
+	split := strings.Split(fullName, ".")
+	fnName := split[len(split)-1]
+	return fnName
+}
+
+func prettyPrint(w io.Writer, x float64, unit string) {
+	// Print all numbers with 10 places before the decimal point
+	// and small numbers with four sig figs. Field widths are
+	// chosen to fit the whole part in 10 places while aligning
+	// the decimal point of all fractional formats.
+	var format string
+	switch y := math.Abs(x); {
+	case y == 0 || y >= 999.95:
+		format = "%10.0f %s"
+	case y >= 99.995:
+		format = "%12.1f %s"
+	case y >= 9.9995:
+		format = "%13.2f %s"
+	case y >= 0.99995:
+		format = "%14.3f %s"
+	case y >= 0.099995:
+		format = "%15.4f %s"
+	case y >= 0.0099995:
+		format = "%16.5f %s"
+	case y >= 0.00099995:
+		format = "%17.6f %s"
+	default:
+		format = "%18.7f %s"
+	}
+	fmt.Fprintf(w, format, x, unit)
+}
+
+func Report(nOps int, ms []*Metric) {
+	buf := new(strings.Builder)
+	fmt.Fprintf(buf, "%-*s", 20, callerName(1))
+	fmt.Fprintf(buf, "\t%8d", nOps)
+	for _, m := range ms {
+		buf.WriteByte('\t')
+		prettyPrint(buf, m.N, m.Unit)
+	}
+	fmt.Println(buf.String())
+}
--- merkle/bench_test.go
+++ merkle/bench_test.go
@@ -3,13 +3,9 @@ package merkle
 import (
 	"bytes"
 	"crypto/sha256"
-	"fmt"
+	"github.com/mit-pdos/pav/benchutil"
 	"github.com/mit-pdos/pav/cryptoffi"
-	"io"
-	"math"
 	"math/rand/v2"
-	"runtime"
-	"strings"
 	"testing"
 	"time"
 )
@@ -34,7 +30,10 @@ func TestBenchRand(t *testing.T) {
 
 	m0 := float64(total.Nanoseconds()) / float64(nOps)
 	m1 := float64(total.Milliseconds())
-	report(nOps, []*metric{{m0, "ns/op"}, {m1, "ms"}})
+	benchutil.Report(nOps, []*benchutil.Metric{
+		{N: m0, Unit: "ns/op"},
+		{N: m1, Unit: "total ms"},
+	})
 }
 
 func TestBenchRandHash(t *testing.T) {
@@ -52,7 +51,10 @@ func TestBenchRandHash(t *testing.T) {
 
 	m0 := float64(total.Nanoseconds()) / float64(nOps)
 	m1 := float64(total.Milliseconds())
-	report(nOps, []*metric{{m0, "ns/op"}, {m1, "ms"}})
+	benchutil.Report(nOps, []*benchutil.Metric{
+		{N: m0, Unit: "ns/op"},
+		{N: m1, Unit: "total ms"},
+	})
 }
 
 func TestBenchGet(t *testing.T) {
@@ -78,7 +80,10 @@ func TestBenchGet(t *testing.T) {
 
 	m0 := float64(total.Nanoseconds()) / float64(nOps)
 	m1 := float64(total.Milliseconds())
-	report(nOps, []*metric{{m0, "ns/op"}, {m1, "ms"}})
+	benchutil.Report(nOps, []*benchutil.Metric{
+		{N: m0, Unit: "ns/op"},
+		{N: m1, Unit: "total ms"},
+	})
 }
 
 func TestBenchProve(t *testing.T) {
@@ -101,7 +106,10 @@ func TestBenchProve(t *testing.T) {
 
 	m0 := float64(total.Nanoseconds()) / float64(nOps)
 	m1 := float64(total.Milliseconds())
-	report(nOps, []*metric{{m0, "ns/op"}, {m1, "ms"}})
+	benchutil.Report(nOps, []*benchutil.Metric{
+		{N: m0, Unit: "ns/op"},
+		{N: m1, Unit: "total ms"},
+	})
 }
 
 func TestBenchPut(t *testing.T) {
@@ -126,7 +134,10 @@ func TestBenchPut(t *testing.T) {
 
 	m0 := float64(total.Nanoseconds()) / float64(nOps)
 	m1 := float64(total.Milliseconds())
-	report(nOps, []*metric{{m0, "ns/op"}, {m1, "ms"}})
+	benchutil.Report(nOps, []*benchutil.Metric{
+		{N: m0, Unit: "ns/op"},
+		{N: m1, Unit: "total ms"},
+	})
 }
 
 func setup(t *testing.T, sz int) (tr *Tree, rnd *rand.ChaCha8) {
@@ -147,63 +158,4 @@ func setup(t *testing.T, sz int) (tr *Tree, rnd *rand.ChaCha8) {
 		}
 	}
 	return
-}
-
-type metric struct {
-	n    float64
-	unit string
-}
-
-// callerName gives the function name for the caller,
-// after skip frames (where 0 means the current function).
-func callerName(skip int) string {
-	pcs := make([]uintptr, 1)
-	callers := runtime.Callers(skip+2, pcs) // skip + runtime.Callers + callerName
-	if callers == 0 {
-		panic("bench: zero callers found")
-	}
-	frames := runtime.CallersFrames(pcs)
-	frame, _ := frames.Next()
-	fullName := frame.Function
-	split := strings.Split(fullName, ".")
-	fnName := split[len(split)-1]
-	return fnName
-}
-
-func prettyPrint(w io.Writer, x float64, unit string) {
-	// Print all numbers with 10 places before the decimal point
-	// and small numbers with four sig figs. Field widths are
-	// chosen to fit the whole part in 10 places while aligning
-	// the decimal point of all fractional formats.
-	var format string
-	switch y := math.Abs(x); {
-	case y == 0 || y >= 999.95:
-		format = "%10.0f %s"
-	case y >= 99.995:
-		format = "%12.1f %s"
-	case y >= 9.9995:
-		format = "%13.2f %s"
-	case y >= 0.99995:
-		format = "%14.3f %s"
-	case y >= 0.099995:
-		format = "%15.4f %s"
-	case y >= 0.0099995:
-		format = "%16.5f %s"
-	case y >= 0.00099995:
-		format = "%17.6f %s"
-	default:
-		format = "%18.7f %s"
-	}
-	fmt.Fprintf(w, format, x, unit)
-}
-
-func report(nOps int, ms []*metric) {
-	buf := new(strings.Builder)
-	fmt.Fprintf(buf, "%-*s", 20, callerName(1))
-	fmt.Fprintf(buf, "\t%8d", nOps)
-	for _, m := range ms {
-		buf.WriteByte('\t')
-		prettyPrint(buf, m.n, m.unit)
-	}
-	fmt.Println(buf.String())
 }
