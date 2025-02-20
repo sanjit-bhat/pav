487bdb3ef756a517b53b600baee7a7037c858123
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-02-20 10:24:16 -0500

    add scaling data up to 237M entries (OOM) from sr4

--- kt/bench_test.go
+++ kt/bench_test.go
@@ -576,7 +576,7 @@ func TestBenchServScale(t *testing.T) {
 	serv, _, _ := NewServer()
 	var seed [32]byte
 	rnd := rand.NewChaCha8(seed)
-	nInsert := 15_000_000
+	nInsert := 237_000_000
 	nMeasure := 500_000
 	var stat runtime.MemStats
 
