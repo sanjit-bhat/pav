10d95d3e5d12ef07b6527cf30f57fb711a0f1990
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-07-07 15:32:30 -0400

    rm adtr from ci serde check

--- etc/ci-check.py
+++ etc/ci-check.py
@@ -5,7 +5,10 @@ from pathlib import Path
 proj_root = Path(__file__).parents[1]
 
 
-# TODO: once compiler supports objects from diff pkgs, test server/serde.go.
+# TODO: once compiler supports objects from diff pkgs, test:
+# {server,auditor}/serde.go.
+
+
 class Tests(unittest.TestCase):
     def test_ktcore_serde(self):
         cmd = "go run ./serde --in ktcore/serde.go && git diff --exit-code ktcore/serde.out.go"
@@ -20,17 +23,6 @@ class Tests(unittest.TestCase):
 
     def test_merkle_serde(self):
         cmd = "go run ./serde --in merkle/serde.go && git diff --exit-code merkle/serde.out.go"
-        res = subprocess.run(
-            cmd, cwd=proj_root, shell=True, capture_output=True, text=True
-        )
-        self.assertEqual(
-            res.returncode,
-            0,
-            f"Command failed with error message: {res.stderr} and output: {res.stdout}",
-        )
-
-    def test_auditor_serde(self):
-        cmd = "go run ./serde --in auditor/serde.go && git diff --exit-code auditor/serde.out.go"
         res = subprocess.run(
             cmd, cwd=proj_root, shell=True, capture_output=True, text=True
         )
