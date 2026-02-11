804e2493941439ef6b24c3d4a17c87fb70755f71
Sanjit Bhat <sanjit.bhat@gmail.com>
2026-02-11 13:35:49 -0500

    key_map: use cryptoffi funcs; get inj and invert for free

--- proof/ktcore_proof/key_map.v
+++ proof/ktcore_proof/key_map.v
@@ -2,11 +2,12 @@ From New.generatedproof.github_com.sanjit_bhat.pav Require Import ktcore.
 From New.proof.github_com.sanjit_bhat.pav Require Import prelude.
 
 From New.proof.github_com.sanjit_bhat.pav Require Import
-  cryptoffi cryptoutil hashchain merkle safemarshal.
+  cryptoffi cryptoutil (* hashchain merkle *) safemarshal.
 
 From New.proof.github_com.sanjit_bhat.pav.ktcore_proof Require Import
   serde.
 
+(*
 Section sep_list2.
   Context {PROP : bi}.
 
@@ -58,6 +59,7 @@ Section sep_list2.
       iFrame "#".
   Qed.
 End sep_list2.
+*)
 
 Module ktcore.
 Import serde.ktcore.
@@ -75,23 +77,26 @@ Section proof.
 Context `{hG: heapGS Σ, !ffi_semantics _ _, !globalsGS Σ} {go_ctx : GoContext}.
 Context `{!pavG Σ}.
 
-Definition is_MapLabel vrf_pk uid ver map_label : iProp Σ :=
+(*
+Definition is_MapLabel vrf_pk uid ver map_label :=
   let enc := MapLabel.pure_enc (MapLabel.mk' uid ver) in
-  cryptoffi.is_vrf_out vrf_pk (Some enc) map_label.
+  cryptoffi.vrf_func vrf_pk enc = Some map_label.
 
 Lemma is_MapLabel_det pk uid ver map_label0 map_label1 :
-  is_MapLabel pk uid ver map_label0 -∗
-  is_MapLabel pk uid ver map_label1 -∗
-  ⌜map_label0 = map_label1⌝.
+  is_MapLabel pk uid ver map_label0 →
+  is_MapLabel pk uid ver map_label1 →
+  map_label0 = map_label1.
 Proof.
   rewrite /is_MapLabel.
-  iIntros "#H0 #H1".
+  intros H0 H1.
   iDestruct (cryptoffi.is_vrf_out_det with "H0 H1") as %->.
   done.
 Qed.
+*)
 
 (* externalize [rand] bc some clients want to determ
 derive [map_val] from [kt_pk]. *)
+(*
 Definition is_MapVal kt_pk rand map_val : iProp Σ :=
   let enc := CommitOpen.pure_enc (CommitOpen.mk' kt_pk rand) in
   cryptoffi.is_hash (Some enc) map_val.
@@ -105,9 +110,10 @@ Proof.
   iDestruct (cryptoffi.is_hash_det with "H0 H1") as %->.
   done.
 Qed.
+*)
 
-Local Definition decode_map_label odata :=
-  rem0 ← odata;
+Local Definition dec_map_label vrf_pk map_label :=
+  rem0 ← cryptoffi.vrf_inv_func vrf_pk map_label;
   guard (length rem0 ≥ 8);;
   let uid := le_to_u64 (take 8 rem0) in
   let rem1 := drop 8 rem0 in
@@ -115,8 +121,8 @@ Local Definition decode_map_label odata :=
   let ver := le_to_u64 (take 8 rem1) in
   Some (uid, uint.nat ver).
 
-Local Definition decode_map_val odata :=
-  rem0 ← odata;
+Local Definition dec_map_val map_val :=
+  rem0 ← cryptoffi.hash_inv_func map_val;
   guard (length rem0 ≥ 8);;
   let pk_len := sint.nat (le_to_u64 (take 8 rem0)) in
   let rem1 := drop 8 rem0 in
@@ -125,29 +131,14 @@ Local Definition decode_map_val odata :=
   (* drop the remaining rand. we don't need that. *)
   Some (pk).
 
-Local Definition is_dec_map_label vrf_pk obj map_label : iProp Σ :=
-  ∃ odata,
-  "#His_vrf_out" ∷ cryptoffi.is_vrf_out vrf_pk odata map_label ∗
-  "%Hdec" ∷ ⌜obj = decode_map_label odata⌝.
+(* easier to reason about list bc might be mult labels that go to None. *)
+Local Definition dec_map_labels vrf_pk hidden : gmap (w64 * nat) (list w8) :=
+  let odec := (λ '(l, v), (dec_map_label vrf_pk l, v)) <$> map_to_list hidden in
+  let dec := omap (λ '(ol, v), l ← ol; Some (l, v)) odec in
+  list_to_map dec.
 
-Local Definition is_dec_map_val obj map_val : iProp Σ :=
-  ∃ odata,
-  "#His_hash" ∷ cryptoffi.is_hash odata map_val ∗
-  "%Hdec" ∷ ⌜obj = decode_map_val odata⌝.
-
-(* we can't big_sepM2 odec and hidden bc their domains are different. *)
-Local Definition is_odec vrf_pk odec hidden : iProp Σ :=
-  ([∗ list] x;y ∈ odec;map_to_list hidden,
-    "#His_label" ∷ is_dec_map_label vrf_pk x.1 y.1 ∗
-    "#His_val" ∷ is_dec_map_val x.2 y.2).
-
-Local Definition filter_Some odec : list (w64 * nat * list w8) :=
-  omap
-    (λ '(olabel, oval),
-      label ← olabel;
-      pk ← oval;
-      Some (label, pk))
-    odec.
+Local Definition dec_map_vals interm : gmap (w64 * nat) (list w8) :=
+  omap (λ v, pk ← dec_map_val v; Some pk) interm.
 
 Local Fixpoint get_contig (m_uid : gmap nat (list w8)) ver fuel :=
   match fuel with 0%nat => [] | S fuel' =>
@@ -158,94 +149,8 @@ Local Definition filter_contig mapped : gmap w64 (list (list w8)) :=
   (* size is simple upper bound on max ver. *)
   (λ m_uid, get_contig m_uid 0 (size m_uid)) <$> mapped.
 
-Definition is_plain_keys (vrf_pk : list w8)
-    (plain : keys_ty) (hidden : gmap (list w8) (list w8)) : iProp Σ :=
-  ∃ odec,
-  "#His_odec" ∷ is_odec vrf_pk odec hidden ∗
-  "%Heq_plain" ∷ ⌜plain = filter_contig $ gmap_curry $ list_to_map $ filter_Some odec⌝.
-
-(* injectivity. *)
-
-Local Lemma is_dec_map_label_inj vrf_pk obj0 obj1 map_label :
-  is_dec_map_label vrf_pk obj0 map_label -∗
-  is_dec_map_label vrf_pk obj1 map_label -∗
-  ⌜obj0 = obj1⌝.
-Proof.
-  iNamedSuffix 1 "0".
-  iNamedSuffix 1 "1".
-  iDestruct (cryptoffi.is_vrf_out_inj with "His_vrf_out0 His_vrf_out1") as %<-.
-  by simplify_eq/=.
-Qed.
-
-Local Lemma is_dec_map_val_inj obj0 obj1 map_val :
-  is_dec_map_val obj0 map_val -∗
-  is_dec_map_val obj1 map_val -∗
-  ⌜obj0 = obj1⌝.
-Proof.
-  iNamedSuffix 1 "0".
-  iNamedSuffix 1 "1".
-  iDestruct (cryptoffi.is_hash_inj with "His_hash0 His_hash1") as %<-.
-  by simplify_eq/=.
-Qed.
-
-Local Lemma is_odec_inj vrf_pk odec0 odec1 hidden :
-  is_odec vrf_pk odec0 hidden -∗
-  is_odec vrf_pk odec1 hidden -∗
-  ⌜odec0 = odec1⌝.
-Proof.
-  iIntros "H0 H1".
-  iDestruct (big_sepL2_det_r with "H0 H1 []") as %<-; [|done].
-  iModIntro. iIntros ([??] [??] ?). simpl.
-  iNamedSuffix 1 "0".
-  iNamedSuffix 1 "1".
-  iDestruct (is_dec_map_label_inj with "His_label0 His_label1") as %<-.
-  by iDestruct (is_dec_map_val_inj with "His_val0 His_val1") as %<-.
-Qed.
-
-(* used by clients who agree across the same digs. *)
-Lemma is_plain_keys_inj vrf_pk plain0 plain1 hidden :
-  is_plain_keys vrf_pk plain0 hidden -∗
-  is_plain_keys vrf_pk plain1 hidden -∗
-  ⌜plain0 = plain1⌝.
-Proof.
-  iNamedSuffix 1 "0". iNamedSuffix 1 "1". subst.
-  by iDestruct (is_odec_inj with "His_odec0 His_odec1") as %<-.
-Qed.
-
-(* inversion. *)
-
-Local Lemma is_dec_map_label_invert vrf_pk map_label :
-  ⊢ ∃ obj, is_dec_map_label vrf_pk obj map_label.
-Proof.
-  iDestruct cryptoffi.is_vrf_out_invert as (?) "$".
-  naive_solver.
-Qed.
-
-Local Lemma is_dec_map_val_invert map_val :
-  ⊢ ∃ obj, is_dec_map_val obj map_val.
-Proof.
-  iDestruct cryptoffi.is_hash_invert as (?) "$".
-  naive_solver.
-Qed.
-
-Local Lemma is_odec_invert vrf_pk hidden :
-  ⊢ ∃ odec, is_odec vrf_pk odec hidden.
-Proof.
-  iApply big_sepL2_invert.
-  iIntros ([??]). simpl.
-  iDestruct is_dec_map_label_invert as (?) "H0".
-  iDestruct is_dec_map_val_invert as (?) "H1".
-  iExists (_, _). iFrame "#".
-Qed.
-
-(* used by auditor.
-to prove this, is_plain_keys can't require bijection. *)
-Lemma is_plain_keys_invert vrf_pk hidden :
-  ⊢ ∃ plain, is_plain_keys vrf_pk plain hidden.
-Proof.
-  iDestruct is_odec_invert as (?) "$".
-  naive_solver.
-Qed.
+Definition plain_inv_func vrf_pk hidden :=
+  filter_contig $ gmap_curry $ dec_map_vals $ dec_map_labels vrf_pk hidden.
 
 (* monotonicity. *)
 
@@ -259,10 +164,9 @@ Proof. Admitted.
 (* NOTE: this lemma requires that pks0@uid@plain0 are prefix of pks1@uid@plain1.
 moving pks0 "up the stack" is easier, since the stack only filters.
 moving pks0 "down the stack" is more tricky.
-first, we remember that pks0 passes filters0.
-some of filters1 (e.g., decode_map_label) are same as filters0.
-second, we prove that the remaining filters (e.g., get_contig)
-are strictly more permissible. *)
+we remember that pks0 passes filters0.
+filter_Some is same in stack0 and stack1.
+filter_contig in stack1 is more permissible than in stack0. *)
 Lemma is_plain_keys_over_sub vrf_pk hidden0 hidden1 plain0 plain1 :
   hidden0 ⊆ hidden1 →
   is_plain_keys vrf_pk plain0 hidden0 -∗
@@ -278,6 +182,7 @@ Definition is_contig (vrf_pk : list w8) (hidden : gmap (list w8) (list w8)) : iP
 Admitted.
 
 (* used in server update. *)
+(*
 Lemma is_plain_keys_add vrf_pk hidden plain uid kt_pk label val rand :
   let pks := plain !!! uid in
   is_plain_keys vrf_pk plain hidden -∗
@@ -288,6 +193,7 @@ Lemma is_plain_keys_add vrf_pk hidden plain uid kt_pk label val rand :
   let plain' := <[uid:=pks ++ [kt_pk]]>plain in
   is_plain_keys vrf_pk plain' hidden' ∗ is_contig vrf_pk hidden'.
 Proof. Admitted.
+*)
 
 End proof.
 End ktcore.
