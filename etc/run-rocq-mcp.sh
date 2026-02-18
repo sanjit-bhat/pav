0f89a97616037d24ba2c524fa208ea91abe33b20
Sanjit Bhat <sanjit.bhat@gmail.com>
2026-02-18 18:35:56 -0500

    tell claude about rocq mcp server

---
+++ etc/run-rocq-mcp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# cd "$1"  # project dir
+ARGS=$(sed -E -e '/^#/d' -e "s/'([^']*)'//g" -e 's/-arg //g' _RocqProject)
+# exec so that MCP client connects directly to rocq-mcp,
+# without indirecting thru this shell script.
+exec rocq-mcp $ARGS
