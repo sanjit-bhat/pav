217f3f0770e22f3dddc241734bb0670d59637e08
Upamanyu Sharma <upamanyu@mit.edu>
2026-03-29 20:32:22 -0400

    Start adding trusted code for crypto

---
+++ trusted_code/github_com/sanjit_bhat/pav/cryptoffi.v
@@ -0,0 +1,14 @@
+From New.golang Require Import defn.
+From Cryptoffi Require Export impl.
+
+#[global]
+Existing Instances crypto_op crypto_model.
+(** * Crypto user-facing operations. *)
+Section crypto.
+  Context {go_gctx : GoGlobalContext}.
+
+  (** Type: func(uint64) Listener *)
+  Definition trustedHashⁱᵐᵖˡ : val :=
+    λ: "data", ExternalOp Hash "data".
+
+End crypto.
